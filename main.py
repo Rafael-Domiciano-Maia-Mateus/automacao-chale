1cbf8314d41c1e8afbebd9112d9a00746a0d91ab
Rafael <rmaia7985@gmail.com>
2024-09-05 20:45:31 -0300

    main.py

--- main.py
+++ main.py
@@ -14,25 +14,37 @@ def localizar_clicar(img, x, y, dalay, conf=0.1):
             break
 
 
+def login():
+    time.sleep(5)
+    if not pyautogui.locateOnScreen(("caso1.png" or "senha.png"), confidence=0.1):
+        pyautogui.alert("Você já está logado.")
+    elif not pyautogui.locateOnScreen("espera.png", confidence=0.1):
+        pyautogui.alert("Aperte 'ok' para fazer login")
+        while True:
+            if not pyautogui.locateOnScreen("caso1.png", confidence=0.1):
+                time.sleep(1)
+            else:
+                time.sleep(1)
+                pyautogui.click(x=954, y=392)
+            
+            if not pyautogui.locateOnScreen("senha.png", confidence=0.1):
+                time.sleep(1)
+            else:
+                time.sleep(1)
+                pyautogui.click(x=947, y=438)
+                break
+        
+
 # Abrir navegador
 pyautogui.hotkey("win", "1")
 
 # Entrar na página dos chalés
 localizar_clicar("pag_chale.png", x=74, y=85, dalay=1)
 
-time.sleep(2)
-
 # Tratar caso de login
-if not pyautogui.locateOnScreen("espera.png", confidence=0.1):
-    if pyautogui.locateOnScreen("caso1.png", confidence=0.1):
-        pyautogui.alert("Vai ter que fazer login, aperte 'ok' para confirmar")
-        while True:
-            # Clicar em confirmar email
-            localizar_clicar("caso1.png", x=954, y=392, dalay=2)
+login()
 
-             # Clicar em confirmar senha
-            localizar_clicar("senha.png", x=947, y=438, dalay=1)
-            break
+time.sleep(2)
 
 # Apertar o botão "financeiro"
 while True:
@@ -40,34 +52,36 @@ while True:
         time.sleep(1)
         break
     else:
-        time.sleep(4)
+        time.sleep(2)
         pyautogui.click(x=1428, y=191)
         break
 
 # Apertar botão de fatura "fatura"
 localizar_clicar("espera1.png", x=1416, y=311, dalay=2)
 
-# Avisar que não tem nenhuma fatura pendente
+# Boleto
 while True:
     if not pyautogui.locateOnScreen("tem_boleto.png", confidence=0.1):
         pyautogui.alert("Não tem nenhum boleto em aberto!")
         break
 
-# Clicar em boleto
-time.sleep(5)
-localizar_clicar("tem_boleto.png", x=451, y=536, dalay=1)
+    elif pyautogui.locateOnScreen("tem_boleto.png", confidence=0.1):
+        # Clicar em boleto
+        time.sleep(5)
+        localizar_clicar("tem_boleto.png", x=451, y=536, dalay=1)
 
-# Linhar o codigo de barras
-pyautogui.click(
-    x=831, 
-    y=123, 
-    clicks=5, 
-    interval=0.25, 
-    button='left'
-)
+        time.sleep(1.5)
 
-pyautogui.scroll(-500)
+        pyautogui.click(
+            x=831, 
+            y=123, 
+            clicks=4, 
+            interval=0.3, 
+            button='left'
+        )
 
-# Fim do código
-time.sleep(2)
+        pyautogui.scroll(-600)
+        break
+
+time.sleep(1.5)
 pyautogui.alert("O código acabou")
