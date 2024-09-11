b5cb17f75098674f6c48054326246b16f740f877
Rafael <rmaia7985@gmail.com>
2024-09-10 21:24:58 -0300

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pyautogui.alert("Não mexa em NADA! Quando o código acabar eu aviso")
 
 pyautogui.PAUSE = 1
 
-def localizar_clicar(img, x, y, dalay, conf=0.1):
+def localizar_clicar(img, x, y, dalay, conf=0.2):
     while True:
         if not pyautogui.locateOnScreen(img, confidence=conf):
             time.sleep(dalay)
@@ -15,25 +15,21 @@ def localizar_clicar(img, x, y, dalay, conf=0.1):
 
 
 def login():
-    time.sleep(5)
-    if not pyautogui.locateOnScreen(("caso1.png" or "senha.png"), confidence=0.1):
-        pyautogui.alert("Você já está logado.")
-    elif not pyautogui.locateOnScreen("espera.png", confidence=0.1):
-        pyautogui.alert("Aperte 'ok' para fazer login")
+    try:
+        time.sleep(4.5)
         while True:
-            if not pyautogui.locateOnScreen("caso1.png", confidence=0.1):
-                time.sleep(1)
+            if pyautogui.locateOnScreen("caso1.png", confidence=0.7):
+                pyautogui.click(x=954, y=421)
+                if pyautogui.locateOnScreen("senha.png", confidence=0.1):
+                    time.sleep(1)
+                    pyautogui.click(x=919, y=441)
+                    break
             else:
-                time.sleep(1)
-                pyautogui.click(x=954, y=392)
-            
-            if not pyautogui.locateOnScreen("senha.png", confidence=0.1):
-                time.sleep(1)
-            else:
-                time.sleep(1)
-                pyautogui.click(x=947, y=438)
                 break
-        
+
+    except Exception as e:
+        pyautogui.alert("Você já está logado.")
+
 
 # Abrir navegador
 pyautogui.hotkey("win", "1")
@@ -45,6 +41,7 @@ localizar_clicar("pag_chale.png", x=74, y=85, dalay=1)
 login()
 
 # Apertar o botão "financeiro"
+time.sleep(2)
 while True:
     if not pyautogui.locateOnScreen("espera.png", confidence=0.1):
         time.sleep(1)
@@ -83,3 +80,4 @@ while True:
 
 time.sleep(1.5)
 pyautogui.alert("O código acabou")
+
